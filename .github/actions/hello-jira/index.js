e1cc24be593c33a35a5b520479e948fac6a4a8c7
Alexander Münch <alexander.muench@senacor.com>
2024-02-19 12:18:53 +0100

    Exemplary posting of two JIRA issues (one with default issue type, one with custom one) - For this demo project and issue type are hardwired

--- .github/actions/hello-jira/index.js
+++ .github/actions/hello-jira/index.js
@@ -1,5 +1,5 @@
 const {getRequiredEnvVariable} = require('./utils');
-const {getJiraIssue} = require('./jira');
+const {getJiraIssue, createJiraIssue} = require('./jira');
 
 const jiraAuth = {
     domain: getRequiredEnvVariable('JIRA_DOMAIN'),
@@ -8,14 +8,53 @@ const jiraAuth = {
 };
 const jiraIssue = getRequiredEnvVariable('JIRA_ISSUE');
 
-getJiraIssue(jiraAuth, jiraIssue)
+const getIssue = getJiraIssue(jiraAuth, jiraIssue)
     .then(jsonData => {
         process.stdout.write(`::debug::Got JSON response: ${JSON.stringify(jsonData)}\n`);
         process.stdout.write(`::notice::Read issue's description: "${jsonData.fields.summary}"\n`);
         process.stdout.write(`::notice::Read issue's status: "${jsonData.fields.status.name}"\n`);
-        process.exit(0);
     })
     .catch(err => {
-        process.stdout.write(`::error::Got error: ${err.message}\n`);
+        process.stdout.write(`::error::Got error: ${err}\n`);
         process.exit(1);
+    });
+
+const createTask = createJiraIssue(
+    jiraAuth,
+    'SEC',
+    'Task',
+    'Hello JIRA',
+    'Hi there.\n\nThis is an automatically posted issue purely over the API :-)'
+)
+    .then(jsonData => {
+        process.stdout.write(`::debug::Got JSON response: ${JSON.stringify(jsonData)}\n`);
+        process.stdout.write('::notice::Successfully created new issue.\n');
+        process.stdout.write(`::notice::New issue has ID ${jsonData.id} and key "${jsonData.key}".\n`);
+    })
+    .catch(err => {
+        process.stdout.write(`::error::Got error: ${err}\n`);
+        process.exit(1);
+    });
+
+const createSecurityIssue = createJiraIssue(
+    jiraAuth,
+    'SEC',
+    'Security issue',
+    'Library FOO is dangerous',
+    '…just kidding, but this is an issue with a custom issue type'
+)
+    .then(jsonData => {
+        process.stdout.write(`::debug::Got JSON response: ${JSON.stringify(jsonData)}\n`);
+        process.stdout.write('::notice::Successfully created new issue.\n');
+        process.stdout.write(`::notice::New issue has ID ${jsonData.id} and key "${jsonData.key}".\n`);
+    })
+    .catch(err => {
+        process.stdout.write(`::error::Got error: ${err}\n`);
+        process.exit(1);
+    });
+
+Promise.all([getIssue, createTask, createSecurityIssue])
+    .then(() => {
+        process.stdout.write('::notice::All JIRA tasks successful.\n');
+        process.exit(0);
     });
