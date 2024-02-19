c2ef6d808aefe46315d6fb03222f685f12dae4fa
Alexander Münch <alexander.muench@senacor.com>
2024-02-19 10:56:42 +0100

    Promise-izing the JIRA code

--- .github/actions/hello-jira/index.js
+++ .github/actions/hello-jira/index.js
@@ -8,39 +8,52 @@ function getRequiredEnvVariable(name) {
         process.exit(1);
     }
 
-    return value
+    return value;
 }
 
-const jiraDomain = getRequiredEnvVariable('JIRA_DOMAIN');
-const jiraEmail = getRequiredEnvVariable('JIRA_EMAIL');
-const jiraToken = getRequiredEnvVariable('JIRA_TOKEN');
+function getJiraIssue(jiraAuth, jiraIssue) {
+    const authorizationBasic = Buffer
+        .from(`${jiraAuth.email}:${jiraAuth.token}`)
+        .toString('base64');
 
+    return new Promise((resolve, reject) => {
+        https.get(`https://${jiraAuth.domain}.atlassian.net/rest/api/2/issue/${jiraIssue}`, {
+            headers: {
+                'Authorization': `Basic ${authorizationBasic}`
+            }
+        }, res => {
+            process.stdout.write(`::notice::Got HTTP status code: ${res.statusCode}\n`);
+
+            let data = '';
+            res.on('data', chunk => {
+                data += chunk;
+            });
+
+            res.on('end', () => {
+                resolve(data);
+            });
+        }).on('error', err => {
+            reject(err);
+        });
+    });
+}
+
+const jiraAuth = {
+    domain: getRequiredEnvVariable('JIRA_DOMAIN'),
+    email: getRequiredEnvVariable('JIRA_EMAIL'),
+    token: getRequiredEnvVariable('JIRA_TOKEN')
+};
 const jiraIssue = getRequiredEnvVariable('JIRA_ISSUE');
 
-const authorizationBasic = Buffer
-    .from(`${jiraEmail}:${jiraToken}`)
-    .toString('base64')
-
-https.get(`https://${jiraDomain}.atlassian.net/rest/api/2/issue/${jiraIssue}`, {
-    headers: {
-        'Authorization': `Basic ${authorizationBasic}`
-    }
-}, res => {
-    process.stdout.write(`::notice::Got HTTP status code: ${res.statusCode}\n`);
-
-    let data = '';
-    res.on('data', chunk => {
-        data += chunk;
-    });
-
-    res.on('end', () => {
+getJiraIssue(jiraAuth, jiraIssue)
+    .then(data => {
         const jsonData = JSON.parse(data);
         process.stdout.write(`::debug::Got JSON response: ${data}\n`);
         process.stdout.write(`::notice::Read issue's description: "${jsonData.fields.summary}"\n`);
         process.stdout.write(`::notice::Read issue's status: "${jsonData.fields.status.name}"\n`);
         process.exit(0);
+    })
+    .catch(err => {
+        process.stdout.write(`::error::Got error: ${err.message}\n`);
+        process.exit(1);
     });
-}).on('error', err => {
-    process.stdout.write(`::error::Got error: ${err.message}\n`);
-    process.exit(1);
-});
