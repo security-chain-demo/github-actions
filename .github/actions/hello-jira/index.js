b83d869de6fd0f2f9f22cc55a760067c69c2e220
Alexander Münch <alexander.muench@senacor.com>
2024-02-19 11:03:27 +0100

    Modularizing the JIRA code

--- .github/actions/hello-jira/index.js
+++ .github/actions/hello-jira/index.js
@@ -1,42 +1,5 @@
-const https = require('node:https');
-
-function getRequiredEnvVariable(name) {
-    const value = process.env[name];
-
-    if (!value) {
-        process.stdout.write(`::warning::Environment variable '${name}' is not set. Aborting.\n`);
-        process.exit(1);
-    }
-
-    return value;
-}
-
-function getJiraIssue(jiraAuth, jiraIssue) {
-    const authorizationBasic = Buffer
-        .from(`${jiraAuth.email}:${jiraAuth.token}`)
-        .toString('base64');
-
-    return new Promise((resolve, reject) => {
-        https.get(`https://${jiraAuth.domain}.atlassian.net/rest/api/2/issue/${jiraIssue}`, {
-            headers: {
-                'Authorization': `Basic ${authorizationBasic}`
-            }
-        }, res => {
-            process.stdout.write(`::notice::Got HTTP status code: ${res.statusCode}\n`);
-
-            let data = '';
-            res.on('data', chunk => {
-                data += chunk;
-            });
-
-            res.on('end', () => {
-                resolve(data);
-            });
-        }).on('error', err => {
-            reject(err);
-        });
-    });
-}
+const {getRequiredEnvVariable} = require('./utils');
+const {getJiraIssue} = require('./jira');
 
 const jiraAuth = {
     domain: getRequiredEnvVariable('JIRA_DOMAIN'),
