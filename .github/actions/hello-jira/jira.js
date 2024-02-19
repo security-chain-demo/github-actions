84e534069aba262bf462946040e5df2b7eb771b4
Alexander Münch <alexander.muench@senacor.com>
2024-02-19 12:18:53 +0100

    Exemplary posting of two JIRA issues (one with default issue type, one with custom one) - For this demo project and issue type are hardwired

--- .github/actions/hello-jira/jira.js
+++ .github/actions/hello-jira/jira.js
@@ -1,14 +1,14 @@
 const https = require('node:https');
 
-function getJiraIssue(jiraAuth, jiraIssue) {
-    const authorizationBasic = Buffer
-        .from(`${jiraAuth.email}:${jiraAuth.token}`)
-        .toString('base64');
+const buildAuthorizationBasic = (jiraAuth) => Buffer
+    .from(`${jiraAuth.email}:${jiraAuth.token}`)
+    .toString('base64');
 
+function getJiraIssue(jiraAuth, jiraIssue) {
     return new Promise((resolve, reject) => {
         https.get(`https://${jiraAuth.domain}.atlassian.net/rest/api/2/issue/${jiraIssue}`, {
             headers: {
-                'Authorization': `Basic ${authorizationBasic}`
+                'Authorization': `Basic ${buildAuthorizationBasic(jiraAuth)}`,
             }
         }, res => {
             process.stdout.write(`::notice::Got HTTP status code: ${res.statusCode}\n`);
@@ -29,4 +29,53 @@ function getJiraIssue(jiraAuth, jiraIssue) {
     });
 }
 
-module.exports = {getJiraIssue};
+function createJiraIssue(jiraAuth, projectKey, issuetypeName, summary, description) {
+    const jsonData = {
+        fields: {
+            project: {
+                key: projectKey
+            },
+            issuetype: {
+                name: issuetypeName
+            },
+            summary,
+            description
+        }
+    };
+
+    return new Promise((resolve, reject) => {
+        const postData = JSON.stringify(jsonData);
+
+        const request = https.request(`https://${jiraAuth.domain}.atlassian.net/rest/api/2/issue`, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Basic ${buildAuthorizationBasic(jiraAuth)}`,
+                'Content-Type': 'application/json'
+            }
+        }, res => {
+            process.stdout.write(`::notice::Got HTTP status code: ${res.statusCode}\n`);
+
+            let data = '';
+            res.on('data', chunk => {
+                data += chunk;
+            });
+
+            res.on('end', () => {
+                const jsonData = JSON.parse(data);
+
+                if (res.statusCode === 201) {
+                    resolve(jsonData);
+                } else {
+                    reject(`Got invalid status code ${res.statusCode}, data = ${data}`);
+                }
+            });
+        }).on('error', err => {
+            reject(err);
+        });
+
+        request.write(postData);
+        request.end();
+    });
+}
+
+module.exports = {getJiraIssue, createJiraIssue};
