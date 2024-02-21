0b274eeee54bae9abd2d4358b4eab8389fe644fd
Alexander Münch <alexander.muench@senacor.com>
2024-02-21 13:16:31 +0100

    Support for assigning a priority in JIRA when creating a new issue - Note: We don't update the priority, so users may assign specific priorities according to the project.

--- .github/actions/sync-trivy-to-jira/utils.js
+++ .github/actions/sync-trivy-to-jira/utils.js
@@ -1,3 +1,10 @@
+/**
+ * Transforms a string `severity` into an int for comparison.
+ * "greater" = "more critical", "lesser" = "less critical".
+ *
+ * @param severity as string
+ * @returns {number}
+ */
 function severityToInt(severity) {
     switch (severity) {
         case 'CRITICAL': return 4;
@@ -9,6 +16,25 @@ function severityToInt(severity) {
     }
 }
 
+/**
+ * Transforms a string `severity` into an int for index access.
+ * 0 = "most critical", "4" = "least critical" (or "unknown").
+ *
+ * @param severity
+ * @returns {number}
+ */
+function severityToIndex(severity) {
+    switch (severity) {
+        case 'CRITICAL': return 0;
+        case 'HIGH': return 1;
+        case 'MEDIUM': return 2;
+        case 'LOW': return 3;
+        case 'UNKNOWN': return 4;
+        default: return 4;
+    }
+}
+
 module.exports = {
-    severityToInt
+    severityToInt,
+    severityToIndex
 };
